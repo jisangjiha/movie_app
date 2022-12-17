ffbadb400089b1c2e24ef6b679e37f950084ade2
Jisang Hong <jisang1024@gmail.com>
2022-12-17 22:56:08 +0900

    update detail

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,21 +1,30 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import Movie from "../components/Movie";
+import "./Detail.css";
 
 function Detail() {
   const { id } = useParams();
-
-  const getMovie = async () => {
+  const [movies, setMovies] = useState([]);
+  const getMovie = useCallback(async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
-  };
+    setMovies(json.data.movie);
+    console.log(json);
+  }, [id]);
   useEffect(() => {
     getMovie();
-  }, []);
+  }, [getMovie]);
   return (
     <div className="container">
-      <h1>Detail</h1>
+      <h1>Detail of "{movies.title}"</h1>
+      <img src={movies.medium_cover_image} alt="coverImg" className="img" />
+      <div>
+        <p>Rating : {movies.rating}</p>
+        <p>Genres : {movies.genres}</p>
+        <p>Language : {movies.language}</p>
+        <p>Year : {movies.year}</p>
+      </div>
     </div>
   );
 }
